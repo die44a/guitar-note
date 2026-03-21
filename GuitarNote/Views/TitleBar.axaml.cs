32d2c1403c0c717f6c95e7f9aabe3d219975da29
dima borisov <dim4ig2007@gmail.com>
2026-03-22 00:16:28 +0500

    small fixes + refactor

--- GuitarNote/Views/TitleBar.axaml.cs
+++ GuitarNote/Views/TitleBar.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.VisualTree;
@@ -17,6 +18,8 @@ public partial class TitleBar : UserControl
         if (DataContext is TitleBarViewModel vm && sender is Button button)
         {
             var window = this.GetVisualRoot() as Window;
+            if (window == null)
+                throw new ArgumentNullException("Window was not found in parent window");
 
             switch (button.Name)
             {
