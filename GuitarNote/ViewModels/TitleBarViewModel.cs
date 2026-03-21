4b6f5ebd5dc7f92d6191b1941f5789fca00d030e
dima borisov <dim4ig2007@gmail.com>
2026-03-22 00:10:38 +0500

    create viewModel for titleBar

--- GuitarNote/ViewModels/TitleBarViewModel.cs
+++ GuitarNote/ViewModels/TitleBarViewModel.cs
@@ -1,6 +1,23 @@
+
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
 namespace GuitarNote.ViewModels;
 
 public class TitleBarViewModel : ViewModelBase
 {
-    
-}
+    public void OnMinimizeButtonClick(Window window)
+    {
+        
+    }
+
+    public void OnCloseButtonClicked(Window window)
+    {
+        
+    }
+
+    public void OnResizeButtonClicked(Window window)
+    {
+        
+    }
+}   
--- GuitarNote/Views/TitleBar.axaml.cs
+++ GuitarNote/Views/TitleBar.axaml.cs
@@ -1,6 +1,7 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using Avalonia.VisualTree;
+using GuitarNote.ViewModels;
 
 namespace GuitarNote.Views;
 
@@ -9,5 +10,26 @@ public partial class TitleBar : UserControl
     public TitleBar()
     {
         InitializeComponent();
+    }
+
+    private void TitleButton_Click(object? sender, RoutedEventArgs e)
+    {
+        if (DataContext is TitleBarViewModel vm && sender is Button button)
+        {
+            var window = this.GetVisualRoot() as Window;
+
+            switch (button.Name)
+            {
+                case "CloseButton":
+                    vm.OnCloseButtonClicked(window);
+                    break;
+                case "ResizeButton":
+                    vm.OnResizeButtonClicked(window);
+                    break;
+                case "MinimizeButton":
+                    vm.OnMinimizeButtonClick(window);
+                    break;
+            }
+        }
     }
 }
