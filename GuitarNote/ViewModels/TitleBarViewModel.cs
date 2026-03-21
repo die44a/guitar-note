065c26c7d5fd6d7421210ee00cdff5ac2a0c6486
dima borisov <dim4ig2007@gmail.com>
2026-03-22 01:37:01 +0500

    refactor click system

--- GuitarNote/ViewModels/TitleBarViewModel.cs
+++ GuitarNote/ViewModels/TitleBarViewModel.cs
@@ -6,19 +6,4 @@ namespace GuitarNote.ViewModels;
 
 public class TitleBarViewModel : ViewModelBase
 {
-    public void OnMinimizeButtonClick(Window window)
-    {
-        window.WindowState = WindowState.Minimized;
-    }
-
-    public void OnCloseButtonClicked(Window window)
-    {
-        window.Close();
-    }
-
-    // TODO: implement this better
-    public void OnResizeButtonClicked(Window window)
-    {
-        window.WindowState = WindowState.Maximized;
-    }
 }   
--- GuitarNote/Views/TitleBar.axaml.cs
+++ GuitarNote/Views/TitleBar.axaml.cs
@@ -1,4 +1,5 @@
 using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
@@ -9,41 +10,56 @@ namespace GuitarNote.Views;
 
 public partial class TitleBar : UserControl
 {
+    private Window? _window;
+    
     public TitleBar()
     {
         InitializeComponent();
         PointerPressed += TitleBar_PointerPressed;
+        DoubleTapped += TitleBar_DoubleClick;
+        
+        AttachedToVisualTree += (s, e) =>
+        {
+            _window = this.GetVisualRoot() as Window;
+            if (_window == null)
+                throw new ArgumentNullException("Window was not found in visual tree");
+        };
     }
 
     private void TitleBar_PointerPressed(object? sender, PointerPressedEventArgs e)
     {
         if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
-        {
-            var window = this.GetVisualRoot() as Window;
-            window?.BeginMoveDrag(e);
-        }
+            _window!.BeginMoveDrag(e);
     }
+
+    private void TitleBar_DoubleClick(object? sender, TappedEventArgs e)
+        => ResizeWindow();
     
     private void TitleButton_Click(object? sender, RoutedEventArgs e)
     {
         if (DataContext is TitleBarViewModel vm && sender is Button button)
         {
-            var window = this.GetVisualRoot() as Window;
-            if (window == null)
-                throw new ArgumentNullException("Window was not found in parent window");
-
             switch (button.Name)
             {
                 case "CloseButton":
-                    vm.OnCloseButtonClicked(window);
+                    _window!.Close();
                     break;
                 case "ResizeButton":
-                    vm.OnResizeButtonClicked(window);
+                    ResizeWindow();  
                     break;
                 case "MinimizeButton":
-                    vm.OnMinimizeButtonClick(window);
+                    MinimizeWindow();
                     break;
             }
         }
     }
+
+    private void MinimizeWindow()
+        =>_window!.WindowState = WindowState.Minimized;
+
+
+    private void ResizeWindow()
+        => _window!.WindowState = _window!.WindowState == WindowState.Maximized 
+            ? WindowState.Normal 
+            : WindowState.Maximized;
 }
