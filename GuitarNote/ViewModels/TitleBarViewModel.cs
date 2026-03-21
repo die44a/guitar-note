4a1f341e857f8542b28381c344ef01b6247eefc5
dima borisov <dim4ig2007@gmail.com>
2026-03-22 00:19:21 +0500

    add implementation of TitleBarViewModel methods

--- GuitarNote/ViewModels/TitleBarViewModel.cs
+++ GuitarNote/ViewModels/TitleBarViewModel.cs
@@ -8,16 +8,17 @@ public class TitleBarViewModel : ViewModelBase
 {
     public void OnMinimizeButtonClick(Window window)
     {
-        
+        window.WindowState = WindowState.Minimized;
     }
 
     public void OnCloseButtonClicked(Window window)
     {
-        
+        window.Close();
     }
 
+    // TODO: implement this better
     public void OnResizeButtonClicked(Window window)
     {
-        
+        window.WindowState = WindowState.Normal;
     }
 }   
